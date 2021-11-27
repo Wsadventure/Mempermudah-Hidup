c37a68f50169a1339ac7e083c459a33b311ba58c
Wsadventure <76592981+Wsadventure@users.noreply.github.com>
2021-11-27 11:23:52 +0700

    Make it so you don't have to manually copy the number.

--- scribd.py
+++ scribd.py
@@ -1,9 +1,15 @@
 import webbrowser
 
-angka_link = input("Masukkan angka yang ada di link Scribdnya : ")
+url = input("Masukkan url scribdnya : ")
+
+#awal code ada di indeks 32 sampai ke indeks 41
+angka_link = url[32:41]
+
 link1 = "https://www.scribd.com/embeds/"
 link2 = "/content?start_page=1&view_mode=scroll&access_key=key-fFexxf7r1bzEfWu3HKwf"
 link = link1 + angka_link + link2
 
 print(f"Page {link} akan dibuka pada browser di window lain")
 webbrowser.open(link)
+
+
